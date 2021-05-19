a2c17655e35aa19cbdb185f79fbfd660c06e443a
ML <mtetno@gmail.com>
2021-05-19 10:41:41 +0530

    Adding staging

--- src/main/java/com/dyteam/testApps/webserver/controller/SubscriptionsController.java
+++ src/main/java/com/dyteam/testApps/webserver/controller/SubscriptionsController.java
@@ -3,13 +3,16 @@ package com.dyteam.testApps.webserver.controller;
 import java.util.Map;
 
 import com.dyteam.testApps.webserver.entity.Subscriptions;
+import com.dyteam.testApps.webserver.entity.User;
 import com.dyteam.testApps.webserver.repository.SubscriptionsRepository;
+import com.dyteam.testApps.webserver.repository.UserRepository;
 import com.dyteam.testApps.webserver.security.LoginUser;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,27 +25,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/subscriptions")
 public class SubscriptionsController {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Autowired
     SubscriptionsRepository subscriptionsRepository;
-     
+
     @PostMapping("/save")
-    public Subscriptions save(@RequestBody Subscriptions subscriptions, @AuthenticationPrincipal final LoginUser loggedInUser) {
+    public Subscriptions save(@RequestBody Subscriptions subscriptions,
+            @AuthenticationPrincipal final LoginUser loggedInUser) {
         logger.info("save Subscriptions = " + subscriptions);
         subscriptions.setAddedBy(loggedInUser.getUserId());
-       return subscriptionsRepository.save(subscriptions);
+
+        Subscriptions savedSubscriptions = subscriptionsRepository.save(subscriptions);
+
+        // Create user
+        User createUser = new User();
+        createUser.setUserType(1);
+        createUser.setCompanyId(savedSubscriptions.getId());
+        createUser.setfName(savedSubscriptions.getUsername());
+        createUser.setlName("");
+        createUser.setEmail(savedSubscriptions.getEmail());
+        createUser.setPassword(passwordEncoder.encode(savedSubscriptions.getPassword()));
+        createUser.setUserName(savedSubscriptions.getUsername());
+        createUser.setContact("");
+        createUser.setAddress("");
+        createUser.setRefUserId(savedSubscriptions.getAddedBy());
+        createUser.setAddedBy(savedSubscriptions.getAddedBy());
+        createUser.setStatus(0);
+
+        userRepository.save(createUser);
+        return savedSubscriptions;
     }
 
-     @PostMapping("/update")
-    public Boolean update(@RequestBody Subscriptions subscriptions, @AuthenticationPrincipal final LoginUser loggedInUser) {
+    @PostMapping("/update")
+    public Boolean update(@RequestBody Subscriptions subscriptions,
+            @AuthenticationPrincipal final LoginUser loggedInUser) {
         logger.info("update Subscriptions = " + subscriptions);
-        subscriptionsRepository.update(subscriptions.getCompanyName(),subscriptions.getUsername(),
-        subscriptions.getPassword(),subscriptions.getEmail(),subscriptions.getTestingEnvironmentId(),
-        subscriptions.getRemindBefore(),subscriptions.getThreads(),subscriptions.getEndDate(),subscriptions.getStartDate(),subscriptions.getId());
+        subscriptionsRepository.update(subscriptions.getCompanyName(), subscriptions.getUsername(),
+                subscriptions.getPassword(), subscriptions.getEmail(), subscriptions.getTestingEnvironmentId(),
+                subscriptions.getRemindBefore(), subscriptions.getThreads(), subscriptions.getEndDate(),
+                subscriptions.getStartDate(), subscriptions.getId());
         return true;
     }
-
 
     @DeleteMapping(value = "/deleteAll")
     public boolean deleteAll(@AuthenticationPrincipal final LoginUser loggedInUser) {
@@ -51,12 +81,12 @@ public class SubscriptionsController {
     }
 
     @DeleteMapping(value = "/delete/{id}")
-    public boolean delete(@AuthenticationPrincipal final LoginUser loggedInUser,@PathVariable(value = "id") Long id) {
-        logger.info("id"+id);
-        subscriptionsRepository.updateBySubscriptionId(loggedInUser.getUserId(),id);
+    public boolean delete(@AuthenticationPrincipal final LoginUser loggedInUser, @PathVariable(value = "id") Long id) {
+        logger.info("id" + id);
+        subscriptionsRepository.updateBySubscriptionId(loggedInUser.getUserId(), id);
         return true;
     }
-    
+
     @GetMapping(value = "/all")
     public Iterable<Map<String, Object>> getAllSubscriptions() {
         logger.info("Inside getAllSubscriptions");
